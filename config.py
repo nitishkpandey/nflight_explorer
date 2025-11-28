94e034212a2e48c16a6fa9a7de2de9855a57af16
Nitish <spywarenitish@gmail.com>
2025-11-28 17:23:29 +0100

    Updated configurations of API

--- config.py
+++ config.py
@@ -11,11 +11,13 @@ if ENV_PATH.exists():
     load_dotenv(ENV_PATH)
 
 # Expose the API key as a constant
-AVIATIONSTACK_API_KEY = os.getenv("AVIATIONSTACK_API_KEY")
+#AVIATIONSTACK_API_KEY = os.getenv("AVIATIONSTACK_API_KEY")
+AIRLABS_API_KEY = os.getenv("AIRLABS_API_KEY")
 
-if not AVIATIONSTACK_API_KEY:
+
+if not AIRLABS_API_KEY:
     # Helpful message if something is wrong
     raise RuntimeError(
-        "AVIATIONSTACK_API_KEY is not set. "
+        "AIRLABS_API_KEY is not set. "
         "Check your .env file in the project root."
     )
